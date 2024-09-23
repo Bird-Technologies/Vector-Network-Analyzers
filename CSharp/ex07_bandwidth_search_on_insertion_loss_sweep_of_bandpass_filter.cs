8134a9565cfa5f53db407ecf78d2b9fb9d1fc210
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2024-09-23 12:55:43 -0400

    Create ex07_bandwidth_search_on_insertion_loss_sweep_of_bandpass_filter.cs
    Yet another example.

---
+++ CSharp/ex07_bandwidth_search_on_insertion_loss_sweep_of_bandpass_filter.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * 
+ Example Description:
+        This example shows how to use remote commands to perform the bandpass 
+        filter characterization steps that are covered in the associated 
+        application note featured on the VNA landing pages on birdrf.com. 
+        Specifically, this includes defining the frequency range of interest, 
+        measuring insertion loss at select points using markers; enabling the 
+        bandwidth search to help evaluate the capability of the bandpass filter 
+        inserted between the two test ports.
+
+@verbatim
+
+The MIT License (MIT)
+
+Copyright (c) 2024 Bird
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@endverbatim
+
+@file ex07_bandwidth_search_on_insertion_loss_sweep_of_bandpass_filter.cs
+ 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NationalInstruments.Visa;
+
+namespace ex07_bandwidth_search_on_insertion_loss_sweep_of_bandpass_filter
+{
+    internal class ex07_bandwidth_search_on_insertion_loss_sweep_of_bandpass_filter
+    {
+        static void Main(string[] args)
+        {
+            var rmSession = new ResourceManager();
+            MessageBasedSession mbSession = (MessageBasedSession)rmSession.Open("TCPIP0::127.0.0.1::inst0::INSTR");
+
+            // Read out any/all errors first.
+            mbSession.RawIO.Write(":SYST:ERR?\n");
+            string[] temp1 = mbSession.RawIO.ReadString().Split(',');
+            while (Convert.ToInt32(temp1[0]) != 0)
+            {
+                mbSession.RawIO.Write(":SYST:ERR?\n");
+                temp1 = mbSession.RawIO.ReadString().Split(',');
+            }
+
+            // Place the VNA in the system default state
+            mbSession.RawIO.Write(":SYST:PRES\n");
+
+            // Set the center frequency for the device
+            mbSession.RawIO.Write(":SENS1:FREQ:CENT 433000000.0\n");
+            // Set the span around the center frequency
+            mbSession.RawIO.Write(":SENS1:FREQ:SPAN 20000000.0\n");
+            // Set the number of measurement points
+            mbSession.RawIO.Write(":SENS1:SWE:POIN 1601\n");
+
+            // Configure for S11, log magnitude format, then auto scale
+            mbSession.RawIO.Write(":CALC1:PAR:COUN 1\n");
+            mbSession.RawIO.Write(":CALC1:PAR1:SEL\n");
+            mbSession.RawIO.Write(":CALC1:PAR1:DEF S21\n");
+            mbSession.RawIO.Write(":CALC1:TRAC1:FORM MLOG\n");
+            mbSession.RawIO.Write(":DISP:WIND1:TRAC1:Y:AUTO\n");
+
+            // Ensure all setup is complete
+            mbSession.RawIO.Write("*OPC?\n");
+            string temp2 = mbSession.RawIO.ReadString();
+
+            // Maximize the trace window
+            mbSession.RawIO.Write(":DISP:WIND1:MAX 1\n");
+
+            // Do a check to see if marker 1 is visible then enable it; set it to the center frqeuncy, then grab the marker measurement data
+            mbSession.RawIO.Write(":CALC1:MARK1?\n");
+            temp2 = mbSession.RawIO.ReadString();
+            mbSession.RawIO.Write(":CALC1:MARK1 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK1:SET CENT\n");
+            mbSession.RawIO.Write(":CALC1:MARK1:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Enable marker 2 then move it to approximatel the lower end then perform a marker search to the loss level of interest
+            mbSession.RawIO.Write(":CALC1:MARK2 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK2:X 428000000.0\n");
+
+            // Find out what frequency the marker searched to then get the data at that point. 
+            mbSession.RawIO.Write(":CALC1:MARK2:X?\n");
+            temp2 = mbSession.RawIO.ReadString();
+            mbSession.RawIO.Write(":CALC1:MARK2:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Enable marker 3 then move it to approximatel the upper end then perform a marker seach to the loss level of interest
+            mbSession.RawIO.Write(":CALC1:MARK3 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:X 438000000.0\n");
+
+            // Find out what frequency the marker searched to then get the data at that point. 
+            mbSession.RawIO.Write(":CALC1:MARK3:X?\n");
+            temp2 = mbSession.RawIO.ReadString();
+            mbSession.RawIO.Write(":CALC1:MARK3:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Enable the bandwidth limit test, apply the type of filter to be evaluated, apply the limit to test to, 
+            // set the reference to markers, then read the test data back to the PC. 
+            mbSession.RawIO.Write(":CALC1:MARK:BWID 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:BWID:TYPE NOTC\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:BWID:THR -3.0\n");
+            mbSession.RawIO.Write(":CALC1:MARK:BWID:REF MARK\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:BWID:DATA?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            mbSession.Dispose();
+            rmSession.Dispose();
+        }
+    }
+}
