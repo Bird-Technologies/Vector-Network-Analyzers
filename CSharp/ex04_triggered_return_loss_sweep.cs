68623903a8bd13e22eac8f355acbf6dba34da1fe
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2024-09-23 11:03:07 -0400

    Create ex04_triggered_return_loss_sweep.cs
    New example to share.

---
+++ CSharp/ex04_triggered_return_loss_sweep.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NationalInstruments.Visa;
+
+namespace ex04_triggered_return_loss_sweep
+{
+    internal class ex04_triggered_return_loss_sweep
+    {
+        static void Main(string[] args)
+        {
+            var rmSession = new ResourceManager();
+            MessageBasedSession mbSession = (MessageBasedSession)rmSession.Open("TCPIP0::127.0.0.1::inst0::INSTR");
+
+            // Read out any/all errors first.
+            mbSession.RawIO.Write(":SYST:ERR?\n");
+            string[] temp1 = mbSession.RawIO.ReadString().Split(',');
+            while (Convert.ToInt32(temp1[0]) != 0)
+            {
+                mbSession.RawIO.Write(":SYST:ERR?\n");
+                temp1 = mbSession.RawIO.ReadString().Split(',');
+            }
+
+            mbSession.RawIO.Write("*IDN?\n");
+            string temp2 = mbSession.RawIO.ReadString();
+            Console.WriteLine("{0}", temp2);
+
+            // Configure for bus triggering. NOTE this is one of the key configuration modifications for this setup. 
+            mbSession.RawIO.Write(":TRIG:SOUR BUS\n");
+
+            // Set the start frequency for the device
+            mbSession.RawIO.Write("SENS1:FREQ:STAR 800000000.0\n");
+            // Set the stop frequency for the device
+            mbSession.RawIO.Write("SENS1:FREQ:STOP 925000000.0\n");
+            // Set the number of measurement points
+            mbSession.RawIO.Write(":SENS1:SWE:POIN 1001\n");
+
+            // Configure for S11, log magnitude format, then auto scale
+            mbSession.RawIO.Write(":CALC1:PAR:COUN 1\n");
+            mbSession.RawIO.Write(":CALC1:PAR1:SEL\n");
+            mbSession.RawIO.Write(":CALC1:PAR1:DEF S11\n");
+            mbSession.RawIO.Write(":CALC1:TRAC1:FORM MLOG\n");
+            mbSession.RawIO.Write(":DISP:WIND1:TRAC1:Y:AUTO\n");
+
+            // Ensure all setup is complete
+            mbSession.RawIO.Write("*OPC?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Trigger a measurement sweep. Again, another key command to inform the triggered operation. 
+            mbSession.RawIO.Write(":TRIG\n");
+
+            // Ensure sweep is complete
+            mbSession.RawIO.Write("*OPC?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Autoscale again for best fit
+            mbSession.RawIO.Write(":DISP:WIND1:TRAC1:Y:AUTO\n");
+
+            // Do a check to see if marker 1 is visible then enable it; set it to the center frqeuncy, then grab the marker measurement data
+            mbSession.RawIO.Write(":CALC1:MARK1?\n");
+            temp2 = mbSession.RawIO.ReadString();
+            mbSession.RawIO.Write(":CALC1:MARK1 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK1:SET CENT\n");
+            mbSession.RawIO.Write(":CALC1:MARK1:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Enable marker 2 then move it to approximatel the lower end cutoff frequency then acquire the marker measurement data
+            mbSession.RawIO.Write(":CALC1:MARK2 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK2:X 824000000.0\n");
+            mbSession.RawIO.Write(":CALC1:MARK2:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            // Enable marker 3 then move it to approximatel the upper end cutoff frequency then acquire the marker measurement data
+            mbSession.RawIO.Write(":CALC1:MARK3 1\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:X 824000000.0\n");
+            mbSession.RawIO.Write(":CALC1:MARK3:Y?\n");
+            temp2 = mbSession.RawIO.ReadString();
+
+            mbSession.Dispose();
+        }
+    }
+}
